cc6827391cfea0aadda6c7c13e6593311c694298
IceSword8492 <cleared.for.approach.runway23@gmail.com>
2019-05-10 16:35:07 +0900

    add u

--- src/main.c
+++ src/main.c
@@ -10,14 +10,14 @@ int main (int argc, const char** argv)
 
 
 	students[0] = (student){117237, "nakamura"}; // def of nakamura
-	students[1] = (student){1, "k"};// students[1] // def of yoshioka
-	// students[2] // def of uesugi
+	// students[1] // def of yoshioka
+	students[2] = (student){2, "u"};// students[2] // def of uesugi
 	// students[3] // def of kumazawa
-	
+
 
 	for (int i = 0; i < COUNT; i++)
 	{
-		printf("C0%d %s\n", students[i].id, students[i].name);	
+		printf("C0%d %s\n", students[i].id, students[i].name);
 	}
 	return 0;
 }
